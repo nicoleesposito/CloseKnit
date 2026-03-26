7b19fc786de03629f74e472d860d80181c322236
Nicole Esposito <nicoleesposito12@icloud.com>
2026-03-26 17:17:00 -0400

    MacOS Port conflict fix - Changed port 5000 to 3000. Was causing issues loading backend on MacOS.

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://backend:5000',
+        target: 'http://backend:3000',
         changeOrigin: true,
       },
     },
